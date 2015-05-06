a5e0e566c45c4d02a1bf10e6991ee8fe8bd644f9
zzyclark <zhangzongyangclark@gmail.com>
2015-05-06 19:13:30 +0800

    test change

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,34 @@
-require 'test_helper'
+require 'test_helper' 	
 
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class ProductTest < ActiveSupport::TestCase                                                                            
+        test "product attributes must not be empty" do                                                                 
+                product = Product.new                                                                                            
+                assert product.invalid?                                                                                      
+                assert product.errors[:title].any?                                                                                 
+                assert product.errors[:description].any?                                                                            
+                assert product.errors[:price].any?                                                                                  
+                assert product.errors[:image_url].any? 
+	end
+		
+	test "product price must be positive" do
+		product = Product.new(title: "book", 
+				description: "asdf", 
+				image_url: "zz.jpg") 
+		product.price = -1
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+		
+		product.price = 0
+		assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
+
+		product.price = 1
+		assert product.valid?
+	end
+	def new_product(image_url)
+		Product.new(title: "bool",
+			    description: "daf",
+			    image_url: image_url)
+	end
 end
+
