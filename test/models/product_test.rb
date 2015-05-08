608281041afcafb7c7f214851281d9f9e847c45d
zzyclark <zhangzongyangclark@gmail.com>
2015-05-08 16:30:07 +0800

    validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,23 +1,24 @@
-require 'test_helper' 	
+require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase                                                                            
-        test "product attributes must not be empty" do                                                                 
-                product = Product.new                                                                                            
-                assert product.invalid?                                                                                      
-                assert product.errors[:title].any?                                                                                 
-                assert product.errors[:description].any?                                                                            
-                assert product.errors[:price].any?                                                                                  
-                assert product.errors[:image_url].any? 
+class ProductTest < ActiveSupport::TestCase
+	fixtures :products
+        test "product attributes must not be empty" do
+                product = Product.new
+                assert product.invalid?
+                assert product.errors[:title].any?
+                assert product.errors[:description].any?
+                assert product.errors[:price].any?
+                assert product.errors[:image_url].any?
 	end
-		
+
 	test "product price must be positive" do
-		product = Product.new(title: "book", 
-				description: "asdf", 
-				image_url: "zz.jpg") 
+		product = Product.new(title: "book",
+				description: "asdf",
+				image_url: "zz.jpg")
 		product.price = -1
 		assert product.invalid?
 		assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
-		
+
 		product.price = 0
 		assert product.invalid?
 		assert_equal "must be greater than or equal to 0.01", product.errors[:price].join('; ')
@@ -30,5 +31,25 @@ class ProductTest < ActiveSupport::TestCase
 			    description: "daf",
 			    image_url: image_url)
 	end
-end
 
+	test "image url" do
+		ok = %w{fred.gif fred.jpg fred.png RED.JPG RED.Jpg
+						http://a.b.c/x/y/z/fesd.gif}
+		bad = %w{fred.doc fred.gif/more fred.gif.more}
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} shouldn't be invalid"
+		end
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+	end
+
+	test "product is not valid without a unique title" do
+		product = Product.new(title: products(:ruby).title,
+													description: "yyy",
+													price: 1,
+													image_url: "fred.gif")
+		assert !product.save
+		assert_equal I18n.translate('errors.messages.taken'), product.errors[:title].join(': ')
+	end
+end
