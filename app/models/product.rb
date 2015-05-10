de7cb7264051e5cc468843cbb53bfc558222c0e8
zzyclark <zhangzongyangclark@gamil.com>
2015-05-11 00:50:21 +0800

    add cart and line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_detory :ensure_not_referenced_by_any_line_item
+
+	private
+		# ensure that there are not line item referenceing this product
+		def ensure_not_referenced_by_any_line_item
+			if line_item.empty?
+				return true
+			else
+				errors.add(:base, 'Line Items present')
+				return false
+			end
+		end
+		
 	validates :title, :description, :image_url, presence: true
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
