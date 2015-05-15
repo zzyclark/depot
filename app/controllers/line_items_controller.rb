dce1b8cc02208170b67ef9c36486a51ff9b345fd
zzyclark <zhangzongyangclark@gmail.com>
2015-05-15 18:02:32 +0800

    ajax cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -33,7 +33,8 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_url }
-        format.json { render :show, status: :created, location: @line_item }
+        format.js
+        format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
