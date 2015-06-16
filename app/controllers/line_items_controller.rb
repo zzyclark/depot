fee17967f8773224b9e8044cd3b6d2b06153079d
zzyclark <zhangzongyangclark@gmail.com>
2015-06-16 13:43:27 +0800

    ajax text add, decrement line item add

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -67,6 +67,27 @@ class LineItemsController < ApplicationController
     end
   end
 
+  #PUT /line_items/1
+  #PUT /line_items/1.json
+  def decrement
+    @cart = current_cart
+
+    @line_item = @cart.line_items.find_by_id(params[:id])
+    @line_item = @line_item.decrement_quantity(@line_item.id)
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to store_path, notice: 'Line item was successfully updated.' }
+        format.js {@current_item = @line_item}
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.js {@current_item = @line_item}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_line_item
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,6 +3,22 @@ class LineItem < ActiveRecord::Base
   belongs_to :cart
 
   def total_price
-    product.price * quantity
+    if product.nil?
+      0
+    else
+      product.price * quantity
+    end
+  end
+
+  def decrement_quantity(line_item_id)
+    current_item = LineItem.find_by_id(line_item_id)
+
+    if current_item.quantity > 1
+      current_item.quantity -= 1
+    else
+      current_item.destroy
+    end
+
+    current_item
   end
 end
