a06aad69dc9b0ed063a7c735ae4a232f22722685
zzyclark <zhangzongyangclark@gmail.com>
2015-06-17 17:45:16 +0800

    add more order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511154411) do
+ActiveRecord::Schema.define(version: 20150617034738) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 20150511154411) do
     t.datetime "updated_at",                                     null: false
     t.integer  "quantity",                           default: 1
     t.decimal  "price",      precision: 8, scale: 2
+    t.integer  "order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string   "name"
+    t.text     "address"
+    t.string   "email"
+    t.string   "pay_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
